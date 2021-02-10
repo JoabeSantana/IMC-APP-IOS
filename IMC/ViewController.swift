c1bf7ffe93630a46a6e9d2939e2e8703aa3582f4
Joabe Santana Correia <joabesantanacorreia@Jobs-MacBook-Pro.local>
2021-02-09 22:39:35 -0300

    Initial commit of the IMC Project Base

---
+++ IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Joabe Santana Correia on 09/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
